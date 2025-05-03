8429116f3f1a5602606a2b880f2d75dc2d223062
frrelsh <shaputraparrel@gmail.com>
2025-05-03 16:37:38 +0700

    osl

---
+++ coba/shm_common.h
@@ -0,0 +1,51 @@
+#ifndef SHM_COMMON_H
+#define SHM_COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <time.h>
+#include <pthread.h>
+
+#define MAX_HUNTERS 50
+#define MAX_DUNGEONS 50
+
+struct Hunter {
+    char username[50];
+    int level;
+    int exp;
+    int atk;
+    int hp;
+    int def;
+    int banned;
+    key_t shm_key;
+};
+
+struct Dungeon {
+    char name[50];
+    int min_level;
+    int exp;
+    int atk;
+    int hp;
+    int def;
+    key_t shm_key;
+};
+
+struct SystemData {
+    struct Hunter hunters[MAX_HUNTERS];
+    int num_hunters;
+    struct Dungeon dungeons[MAX_DUNGEONS];
+    int num_dungeons;
+    int current_notification_index;
+};
+
+key_t get_system_key() {
+    return ftok("/tmp", 'S');
+}
+
+#endif
